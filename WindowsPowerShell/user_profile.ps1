3ee04caaf11a7c0681df187a006c0bc52f542e40
/TKKCODE <109029074+TKKCODE@users.noreply.github.com>
2022-09-09 22:11:14 +0530

    Add files via upload

---
+++ WindowsPowerShell/user_profile.ps1
@@ -0,0 +1,12 @@
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+Import-Module posh-git
+Import-Module oh-my-posh
+$omp_config = Join-Path $PSScriptRoot ".\takuya.omp.json"
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+#Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
